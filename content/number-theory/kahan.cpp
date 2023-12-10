6119df2b9d1049694da02ad92ed3645c68cc7b8f
matistjati <matistjati@outlook.com>
2023-12-11 00:32:12 +0100

    Updated kahan

--- content/number-theory/kahan.cpp
+++ content/number-theory/kahan.cpp
@@ -1,13 +1,17 @@
-struct kahan
+// Compiler might optimize out the algorithm, consider disabling ofast
+typedef double real;
+struct Real
 {
-    double sm, c;
-    kahan() : sm(0.0), c(0.0) { }
-    void add_val(double d)
+    real x, c;
+    Real() : x(0), c(0) {}
+    Real(real v) : x(v), c(0) {}
+    Real operator+(real d)
     {
         double y = d - c;
-        double t = sm + y;
-        c = (t - sm) - y;
-        sm = t;
+        double t = x + y;
+        c = (t - x) - y;
+        x = t;
     }
-    double get_sum() { return sm; }
+    double get() { return x; }
 };
+
