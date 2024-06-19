fc629194f7281ed08f55fa31c9c7681dde6d1658
matistjati <matistjati@outlook.com>
2024-06-19 23:58:31 +0200

    Improved edmonds slightly

--- content/graph/edmonds.cpp
+++ content/graph/edmonds.cpp
@@ -1,61 +1,67 @@
 struct Edmonds
 {
-    vvi flow;
-    vvi capacity;
-    vi par;
-    int n;
-    int maxflow = 0;
+	vvi flow;
+	vvi capacity;
+	vi par;
+	int n;
+	int tot_flow = 0;
 
-    void addEdge(int a, int b, int c)
-    {
-        capacity[a][b] = c;
-    }
+	void addEdge(int a, int b, int c)
+	{
+		capacity[a][b] = c;
+	}
 
-    Edmonds(int n) : n(n), flow(n, vi(n)), capacity(n, vi(n)), par(n) {}
+	Edmonds(int n) : n(n), flow(n, vi(n)), capacity(n, vi(n)), par(n) {}
 
-    int bfs(int s, int t)
-    {
-        rep(i, n) par[i] = -1;
-        par[s] = -2;
-        queue<p2> q;
-        q.emplace(s, inf);
+	int bfs(int s, int t)
+	{
+		rep(i, n) par[i] = -1;
+		par[s] = -2;
+		queue<p2> q;
+		q.emplace(s, inf);
 
-        while (q.size())
-        {
-            int u, f;
-            tie(u, f) = q.front(); q.pop();
+		while (q.size())
+		{
+			int u, f;
+			tie(u, f) = q.front(); q.pop();
 
-            rep(e, n)
-            {
-                if (par[e] == -1 && flow[u][e] < capacity[u][e])
-                {
-                    par[e] = u;
-                    int newflow = min(f, capacity[u][e] - flow[u][e]);
+			rep(e, n)
+			{
+				if (par[e] == -1 && flow[u][e] < capacity[u][e])
+				{
+					par[e] = u;
+					int newflow = min(f, capacity[u][e] - flow[u][e]);
 
-                    if (e == t) return newflow;
-                    q.emplace(e, newflow);
-                }
-            }
-        }
+					if (e == t) return newflow;
+					q.emplace(e, newflow);
+				}
+			}
+		}
 
-        return 0;
-    }
+		return 0;
+	}
 
-    int augment(int s, int t) // find one augmenting path
-    {
-        int f = 0;
-        if (f = bfs(s, t))
-        {
-            maxflow += f;
-            for (int cur = t; cur != s; cur = par[cur])
-            {
-                int p = par[cur];
-                flow[p][cur] += f;
-                flow[cur][p] -= f;
-            }
+	int augment(int s, int t) // find one augmenting path
+	{
+		int f = 0;
+		if (f = bfs(s, t))
+		{
+			tot_flow += f;
+			for (int cur = t; cur != s; cur = par[cur])
+			{
+				int p = par[cur];
+				flow[p][cur] += f;
+				flow[cur][p] -= f;
+			}
 
-            return 1;
-        }
-        return 0;
-    }
+			return 1;
+		}
+		return 0;
+	}
+
+	int maxflow(int s, int t)
+	{
+		while(augment(s,t)) {}
+		return tot_flow;
+	}
 };
