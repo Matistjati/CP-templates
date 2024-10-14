2de8999d6277a55ca6b4d1524fb50fc57729dd72
Matistjati <matistjati@outlook.com>
2024-10-14 20:23:44 +0200

    Improve modint

--- content/number-theory/modint.cpp
+++ content/number-theory/modint.cpp
@@ -1,4 +1,3 @@
-// Status: all operations but / are tested
 ll euclid(ll a, ll b, ll& x, ll& y) {
     if (!b) return x = 1, y = 0, a;
     ll d = euclid(b, a % b, y, x);
@@ -13,12 +12,14 @@ struct Mint {
     Mint() : x(0) {}
     Mint operator+(Mint b) { int y = x + b.x; return { y - (y >= mod) * mod, false }; }
     Mint operator-(Mint b) { int y = x - b.x; return { y + (y < 0) * mod, false }; }
-    Mint operator-() { return { -x+mod, false }; }
+    Mint operator-() { return { -x + mod, false }; }
     Mint operator*(Mint b) { return { x * b.x % mod, false }; }
-    Mint operator/(Mint b) { return { x * invert(b) % mod, false }; }
+    Mint operator/(Mint b) { return { x * invert(b).x % mod, false }; }
     Mint invert(Mint a) {
         ll x, y, g = euclid(a.x, mod, x, y);
         assert(g == 1); return { (x + mod) % mod };
     }
+    friend ostream& operator<<(ostream& out, const Mint& a) { return out << a.x; }
+    friend istream& operator>>(istream& in, Mint& a) { ll val; in >> val; a = Mint(val); return in; }
 };
 typedef Mint<int(1e9 + 7)> Mod;
