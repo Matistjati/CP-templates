176ecc0f61157ebbd615cba3ebc2ded7a1721caa
Matistjati <matistjati@outlook.com>
2024-10-18 03:43:26 +0200

    Yet another ILP upgrade

--- content/numerical/ILP.cpp
+++ content/numerical/ILP.cpp
@@ -1,5 +1,6 @@
 // Max ILP solver, use with KACTL LP solver
 
+// ILP. Maximize
 bool is_integer(double x)
 {
 	return abs(x - floor(x)) < eps;
@@ -17,10 +18,12 @@ bool is_feasible(vd& x, vvd& A, vd& b, vi& integer_constraint)
 		rep(j, sz(x)) v += x[j] * A[i][j];
 		if (v > b[i]) return false;
 	}
-
 	return true;
 }
 
+// notes: don't forget to set time_limit, depth, and integer_constraint
+const int time_limit = 950;
+auto start = chrono::high_resolution_clock::now();
 double global_lb = -inf;
 vvd A;
 vd b, c;
@@ -75,11 +78,15 @@ struct node
 	}
 	void branch(int d)
 	{
-		if (d > 250)
+		if (d > 75)
 		{
 			return;
 		}
 		if (ub < global_lb)
+		{
+			return;
+		}
+		if (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start).count() > time_limit)
 		{
 			return;
 		}
@@ -106,6 +113,7 @@ struct node
 				children.emplace_back(new node(v, 0LL));
 				b.pop_back();
 				A.pop_back();
+				break;
 			}
 		}
 		// branch on largest children first
