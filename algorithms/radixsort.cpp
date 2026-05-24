ec5d101999e9d45c29a5b6b9ab1cc9def6969b1c
Matistjati <matistjati@outlook.com>
2026-05-24 05:15:58 +0200

    Nicer radix sort

--- algorithms/radixsort.cpp
+++ algorithms/radixsort.cpp
@@ -1,25 +1,24 @@
 
-typedef tuple<int, int, int> T;
+using T = tuple<int, int, int>;
+int get_(const T& x) {return get<0>(x);}
 void radix_sort_pairs(vector<T>& a) {
     const int B = 8;
     const int MASK = (1 << B) - 1;
     const int PASSES = (32 + B - 1) / B;
 
     vector<T> tmp(a.size());
+    int pref[1 << B];
     for (int pass = 0; pass < PASSES; pass++) {
         int shift = pass * B;
 
         int cnt[1 << B] = { 0 };
-        for (auto& x : a)
-            cnt[(get<0>(x) >> shift) & MASK]++; // change this
+        for (auto& x : a) cnt[(get_(x) >> shift) & MASK]++;
 
-        int pref[1 << B];
         pref[0] = 0;
-        for (int i = 1; i < (1 << B); i++)
-            pref[i] = pref[i - 1] + cnt[i - 1];
+        for (int i = 1; i < (1 << B); i++) pref[i] = pref[i - 1] + cnt[i - 1];
 
         for (auto& x : a) {
-            int d = (get<0>(x) >> shift) & MASK; // and this
+            int d = (get_(x) >> shift) & MASK;
             tmp[pref[d]++] = x;
         }
         a.swap(tmp);
